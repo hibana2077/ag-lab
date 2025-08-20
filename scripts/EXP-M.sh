e65255cb22fa57211180345fcffd02c05bbe8422
hibana2077 <hibana2077@gmail.com>
2025-08-20 18:28:10 +0800

    feat: Add initial job script for benchmarking with CUDA

---
+++ scripts/EXP-M.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#PBS -P rp06
+#PBS -q gpuvolta
+#PBS -l ngpus=1            
+#PBS -l ncpus=12
+#PBS -l mem=8GB           
+#PBS -l walltime=05:30:00  
+#PBS -l wd                  
+#PBS -l storage=scratch/rp06
+
+module load cuda/12.6.2
+# module load python3/3.10.4
+
+source /scratch/rp06/sl5952/ag-lab/.venv/bin/activate
+
+cd ..
+python3 benchmark.py --task needle --model mamba --context_len 256 --epochs 300 >> M001.log
+python3 benchmark.py --task needle --model mamba --context_len 512 --epochs 300 >> M002.log
+python3 benchmark.py --task needle --model mamba --context_len 1024 --epochs 300 >> M003.log
